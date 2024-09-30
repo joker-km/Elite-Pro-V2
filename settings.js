91cd4468c63feec6859a336e389657315d2a386b
joker-km <jokerbig87@gmail.com>
2024-09-30 11:14:57 +0100

    settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.location = "Port Harcourt, Nigeria" //ur location
 
 //new
 global.botname = 'ELITE-PRO-V2' //ur bot name
-global.ownernumber = '2348109263390' //ur owner number
+global.ownernumber = '237656828684' //ur owner number
 global.ownername = 'ELITE-PRO-V2' //ur owner name
 global.websitex = "https://youtu.be/@elite-pro-empire"
 global.wagc = "https://whatsapp.com/channel/0029VaXaqHII1rcmdDBBsd3g"
